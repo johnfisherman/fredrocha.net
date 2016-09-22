a42329ced7419362990df173aab6932feb027fec
Fred Rocha <john.fisherman@gmail.com>
2016-09-22 11:23:26 +0100

    Hide footer from Home.

--- footer.php
+++ footer.php
@@ -1,25 +1,29 @@
-			<footer class="footer" role="contentinfo">
+			<?php if(!is_home()):		?>
+		
+				<footer class="footer" role="contentinfo">
 
-				<div id="inner-footer" class="wrap clearfix">
+					<div id="inner-footer" class="wrap clearfix">
 
-					<div id="social">
-						<ul class="soc">
-					    <li><a class="soc-twitter" href="http://www.twitter.com/john_fisherman" target="_blank"></a></li>
-					    <li><a class="soc-rss" href="<?php bloginfo('url'); ?>/feed" target="_blank"></a></li>
-					    <li><a class="soc-github" href="http://www.github.com/johnfisherman" target="_blank"></a></li>
-					    <li><a class="soc-linkedin" href="http://linkedin.com/in/fredrocha/" target="_blank"></a></li>
-					    <li><a class="soc-email1 soc-icon-last" href="mailto:john.fisherman@gmail.com" target="_blank"></a></li>
-						</ul>
-					</div>
-					<nav role="navigation">
-							<?php bones_footer_links(); ?>
-					</nav>
+						<div id="social">
+							<ul class="soc">
+						    <li><a class="soc-twitter" href="http://www.twitter.com/john_fisherman" target="_blank"></a></li>
+						    <li><a class="soc-rss" href="<?php bloginfo('url'); ?>/feed" target="_blank"></a></li>
+						    <li><a class="soc-github" href="http://www.github.com/johnfisherman" target="_blank"></a></li>
+						    <li><a class="soc-linkedin" href="http://linkedin.com/in/fredrocha/" target="_blank"></a></li>
+						    <li><a class="soc-email1 soc-icon-last" href="mailto:john.fisherman@gmail.com" target="_blank"></a></li>
+							</ul>
+						</div>
+						<nav role="navigation">
+								<?php bones_footer_links(); ?>
+						</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
 
-				</div> <?php // end #inner-footer ?>
+					</div> <?php // end #inner-footer ?>
 
-			</footer> <?php // end footer ?>
+				</footer> <?php // end footer ?>
+
+			<?php endif ?>
 
 		</div> <?php // end #container ?>
 
