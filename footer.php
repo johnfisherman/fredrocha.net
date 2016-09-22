2d000d58bdecfed7f103ea4bf5e4ca730c6aa310
Fred Rocha <john.fisherman@gmail.com>
2016-09-22 11:11:27 +0100

    Add social buttons to footer.

--- footer.php
+++ footer.php
@@ -1,14 +1,21 @@
 			<footer class="footer" role="contentinfo">
 
 				<div id="inner-footer" class="wrap clearfix">
-				<div id="social">
-					<a href="<?php bloginfo('url'); ?>/feed">RSS</a>
-				</div>
+
+					<div id="social">
+						<ul class="soc">
+					    <li><a class="soc-twitter" href="http://www.twitter.com/john_fisherman" target="_blank"></a></li>
+					    <li><a class="soc-rss" href="<?php bloginfo('url'); ?>/feed" target="_blank"></a></li>
+					    <li><a class="soc-github" href="http://www.github.com/johnfisherman" target="_blank"></a></li>
+					    <li><a class="soc-linkedin" href="http://linkedin.com/in/fredrocha/" target="_blank"></a></li>
+					    <li><a class="soc-email1 soc-icon-last" href="mailto:john.fisherman@gmail.com" target="_blank"></a></li>
+						</ul>
+					</div>
 					<nav role="navigation">
 							<?php bones_footer_links(); ?>
 					</nav>
 
-					<!-- <p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p> -->
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
 
 				</div> <?php // end #inner-footer ?>
 
