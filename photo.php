ff88d01c610ca187c1505a927b791a9bdd7d7af4
Fred Rocha <john.fisherman@gmail.com>
2021-02-24 19:29:58 +0000

    Complete new photos page. Fetch photos from Pexels, given an array of IDs.

--- photo.php
+++ photo.php
@@ -8,21 +8,42 @@ Template Name: Photo
 
 			<div id="content" class="photo-content clearfix">
 
-				<div id="inner-content" class="packery-container"> 
+				<div id="inner-content" class="packery-container">
 
 						<div id="main" class="first clearfix" role="main">
 
-							<p>
-								<a class="fancybox area" href="<?php bloginfo(template_url); ?>/img/monica.jpg" data-fancybox-group="gallery" title="Italia, 2013"><img src="<?php bloginfo(template_url); ?>/img/monica small.JPG" alt="" /></a>
+							<?php
 
-								<a class="fancybox area" href="<?php bloginfo(template_url); ?>/img/bidimensional relief.JPG" data-fancybox-group="gallery" title="Bienale di Venezia, Italia, 2013"><img src="<?php bloginfo(template_url); ?>/img/bidimensional relief small.JPG" alt="" /></a>
+								// use GuzzleHttp\Client;
+								//
+								// $pexelsClient = new Client([
+								//     // Base URI is used with relative requests
+								//     'base_uri' => 'http://httpbin.org',
+								//     // You can set any number of default request options.
+								//     'timeout'  => 2.0,
+								// ]);
 
-								<a class="fancybox area" href="<?php bloginfo(template_url); ?>/img/spacious garments.JPG" data-fancybox-group="gallery" title="Venezia, Italia, 2013"><img src="<?php bloginfo(template_url); ?>/img/spacious garments small.JPG" alt="" /></a>
+								// IDs come in a can, they were put there by a man, in a factory downtown
+								// https://www.pexels.com/photo/landscape-red-kitchen-fruit-6876155/
+								$photoIDs = array(6876154, 6768035, 6768036, 6876155);
 
-								<a class="fancybox area" href="<?php bloginfo(template_url); ?>/img/wall of time.JPG" data-fancybox-group="gallery" title="Venezia, Italia, 2013"><img src="<?php bloginfo(template_url); ?>/img/wall of time small.JPG" alt="" /></a>
+								foreach ($photoIDs as $currentPhoto) {
 
-							</p>
+							 ?>
 
+							 	<img class="freds-photo" src="https://images.pexels.com/photos/<?php echo $currentPhoto; ?>/pexels-photo-<?php echo $currentPhoto; ?>.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Photo by Fred Rocha, taken from Pexels">
+
+					 		 <?php } ?>
+
+							 <p id="photo-colophon">
+								 
+								 Photos by Fred Rocha, hosted on
+
+								 <a href="https://www.pexels.com" target="_blank">
+									 <img src="https://images.pexels.com/lib/api/pexels-white.png" />
+								 </a>
+
+						 	</p>
 
 						</div> <?php // end #main ?>
 
