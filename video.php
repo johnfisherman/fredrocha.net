549ff17543867a4012a7112ab6f01799c9e1fb57
Fred Rocha <john.fisherman@gmail.com>
2022-04-28 10:06:29 +0100

    Add Calypsol to video page. Replace All with Calypsol on homepage.

--- video.php
+++ video.php
@@ -14,6 +14,8 @@ Template Name: Video
 
 							<!-- <h1>Videos I have shot along the years</h1> -->
 
+							<div class="single-video-embed" style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/702529770?color=01d4f&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+
 							<div class="single-video-embed" style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/479891413?color=01d4f&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 
 							<div class="single-video-embed" style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1181414?color=01d4f&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
@@ -21,7 +23,7 @@ Template Name: Video
 							<div class="single-video-embed" style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/1181219?color=01d4f&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 
 							<p id="video-colophon">
-								All videos hosted on <a href="https://vimeo.com/fredrocha" target="_blank">Vimeo</a>.
+								All videos hosted on <a href="https://vimeo.com/fredrocha" target="_blank">Vimeo</a>. Visit my <a href="http://www.youtube.com/FredRochaNet" target="_blank">YouTube channel</a> if you happen to spend more time there. Just beware of The Algorithm™, kay?
 				 			</p>
 
 						</div> <?php // end #main ?>
