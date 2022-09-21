5e9a5bde8078fef35f1871e74dc5b30f347eb588
Fred Rocha <john.fisherman@gmail.com>
2022-09-21 12:20:44 +0200

    First releasable version of new webdev page. Added becabe.ca and lusithanddevices.com to list of projects. Updated CV link.

--- webdev.php
+++ webdev.php
@@ -6,6 +6,8 @@ Template Name: WebDev
 
 <?php get_header(); ?>
 
+<?php $templateURL = get_bloginfo('template_url'); ?>
+
 	<div id="content" class="webdev-content clearfix">
 
 		<div id="inner-content">
@@ -14,14 +16,15 @@ Template Name: WebDev
 
 					<h1>Web Development</h1>
 
-					<p>The websites I build with people are meant to be fast, practical and easy to maintain and update.</p>
+					<p class="webdev-intro">The websites I build with people are meant to be fast, practical and easy to maintain and update. Tapping into the power
+					of the WordPress ecosystem, clients are independent to upload and manage content on their end.</p>
 
-					<p>This a small selection of projects I have worked on recently.</p>
+					<p class="webdev-intro">This a small selection of projects I have worked on recently.</p>
 
 					<div class="website threecol">
-						<a href="https://madalenamarques.com" rel="external" target="_blank"><img src="<?php bloginfo('template_url') ?>/img/webdev-mmarques-s.jpg" alt="Photo of Leonor, the actress for 'O Homem das Mil Moradas', holding a picture of Gulbenkian to her face, pretending to be him."></a>
+						<a href="https://madalenamarques.com" rel="external" target="_blank"><img src="<?php echo $templateURL; ?>/img/webdev-mmarques-s.jpg" alt="Photo of Leonor, the actress for 'O Homem das Mil Moradas', holding a picture of Gulbenkian to her face, pretending to be him."></a>
 						<p>
-							Madalena's website showcases her work as a document-based theatre creator for young and old audiences, as well as her work as a cultural mediator.
+							Madalena's website showcases her work as a theatre creator for young and old audiences, as well as her work as a cultural mediator.
 						</p>
 						<p class="website-url">
 							<svg viewBox="0 0 24 24"><path d="m0 0h24v24h-24z" opacity="0"/><g><path d="m8 12a1 1 0 0 0 1 1h6a1 1 0 0 0 0-2h-6a1 1 0 0 0 -1 1z"/><path d="m9 16h-1.79a4.13 4.13 0 0 1 -4.21-3.63 4 4 0 0 1 4-4.37h2a1 1 0 0 0 0-2h-1.79a6.15 6.15 0 0 0 -6.16 5.21 6 6 0 0 0 5.95 6.79h2a1 1 0 0 0 0-2z"/><path d="m23 11.24a6.16 6.16 0 0 0 -6.24-5.24h-1.51c-.81 0-1.25.45-1.25 1a1 1 0 0 0 1 1h1.79a4.13 4.13 0 0 1 4.21 3.63 4 4 0 0 1 -4 4.37h-2a1 1 0 0 0 0 2h2a6 6 0 0 0 6-6.76z"/></g></svg>
@@ -30,9 +33,20 @@ Template Name: WebDev
 					</div>
 
 					<div class="website threecol">
-						<a href="https://fumaca.pt" rel="external" target="_blank"><img src="<?php bloginfo('template_url') ?>/img/webdev-fumaca-s.jpg" alt="Gustavo Ciríaco is a multi-faceted artist. Performer, dancer, composer, director. Living between Brazil and Portugal."></a>
+						<a href="https://becabe.ca" target="_blank"><img src="<?php echo $templateURL; ?>/img/webdev-becabeca-s.jpg" alt="The main character of Anete Melece's movie 'The Kiosk' peeks from within a bunch of magazines and books."></a>
 						<p>
-							Fumaça is a progressive and dissident, community-supported podcast that produces slow-journalism pieces. The website hosts interviews, articles and podcast episodes, besides driving subscriptions and newsletter signups.
+							Quality digital content for kids and families selected by a team of curators, themselves creating for families (illustrators, writers, publishers).
+						</p>
+						<p class="website-url">
+							<svg viewBox="0 0 24 24"><path d="m0 0h24v24h-24z" opacity="0"/><g><path d="m8 12a1 1 0 0 0 1 1h6a1 1 0 0 0 0-2h-6a1 1 0 0 0 -1 1z"/><path d="m9 16h-1.79a4.13 4.13 0 0 1 -4.21-3.63 4 4 0 0 1 4-4.37h2a1 1 0 0 0 0-2h-1.79a6.15 6.15 0 0 0 -6.16 5.21 6 6 0 0 0 5.95 6.79h2a1 1 0 0 0 0-2z"/><path d="m23 11.24a6.16 6.16 0 0 0 -6.24-5.24h-1.51c-.81 0-1.25.45-1.25 1a1 1 0 0 0 1 1h1.79a4.13 4.13 0 0 1 4.21 3.63 4 4 0 0 1 -4 4.37h-2a1 1 0 0 0 0 2h2a6 6 0 0 0 6-6.76z"/></g></svg>
+							<a href="https://becabe.ca" target="_blank">https://becabe.ca</a>
+						</p>
+					</div>
+
+					<div class="website threecol">
+						<a href="https://fumaca.pt" rel="external" target="_blank"><img src="<?php echo $templateURL; ?>/img/webdev-fumaca-s.jpg" alt="Gustavo Ciríaco is a multi-faceted artist. Performer, dancer, composer, director. Living between Brazil and Portugal."></a>
+						<p>
+							Fumaça is a progressive, community-supported podcast that produces slow-journalism pieces. The website hosts interviews, articles and podcast episodes, besides driving subscriptions and newsletter signups.
 						</p>
 						<p class="website-url">
 							<svg viewBox="0 0 24 24"><path d="m0 0h24v24h-24z" opacity="0"/><g><path d="m8 12a1 1 0 0 0 1 1h6a1 1 0 0 0 0-2h-6a1 1 0 0 0 -1 1z"/><path d="m9 16h-1.79a4.13 4.13 0 0 1 -4.21-3.63 4 4 0 0 1 4-4.37h2a1 1 0 0 0 0-2h-1.79a6.15 6.15 0 0 0 -6.16 5.21 6 6 0 0 0 5.95 6.79h2a1 1 0 0 0 0-2z"/><path d="m23 11.24a6.16 6.16 0 0 0 -6.24-5.24h-1.51c-.81 0-1.25.45-1.25 1a1 1 0 0 0 1 1h1.79a4.13 4.13 0 0 1 4.21 3.63 4 4 0 0 1 -4 4.37h-2a1 1 0 0 0 0 2h2a6 6 0 0 0 6-6.76z"/></g></svg>
@@ -41,7 +55,7 @@ Template Name: WebDev
 					</div>
 
 					<div class="website threecol">
-						<a href="http://gustavociria.co" target="_blank"><img src="<?php bloginfo('template_url') ?>/img/webdev-gus-s.jpg" alt="Gustavo Ciríaco is a multi-faceted artist. Performer, dancer, composer, director. Living between Brazil and Portugal."></a>
+						<a href="http://gustavociria.co" target="_blank"><img src="<?php echo $templateURL; ?>/img/webdev-gus-s.jpg" alt="Gustavo Ciríaco is a multi-faceted artist. Performer, dancer, composer, director. Living between Brazil and Portugal."></a>
 						<p>
 							Gustavo Ciríaco is a dancer, choreographer and performing artist and his website serves as a display for his work; present, past and future.
 						</p>
@@ -51,13 +65,34 @@ Template Name: WebDev
 						</p>
 					</div>
 
+					<div class="website threecol">
+						<a href="https://lusithanddevices.com/" target="_blank"><img src="<?php echo $templateURL; ?>/img/webdev-lusithand-s.jpg" alt="Gustavo Ciríaco is a multi-faceted artist. Performer, dancer, composer, director. Living between Brazil and Portugal."></a>
+						<p>
+							Lusithand Devices make bespoke, high-quality bass pre-amps and pedals and through their e-commmerce will <strong>soon</strong> be able to take orders from musicians and music retail stores.
+						</p>
+						<p class="website-url">
+							<svg viewBox="0 0 24 24"><path d="m0 0h24v24h-24z" opacity="0"/><g><path d="m8 12a1 1 0 0 0 1 1h6a1 1 0 0 0 0-2h-6a1 1 0 0 0 -1 1z"/><path d="m9 16h-1.79a4.13 4.13 0 0 1 -4.21-3.63 4 4 0 0 1 4-4.37h2a1 1 0 0 0 0-2h-1.79a6.15 6.15 0 0 0 -6.16 5.21 6 6 0 0 0 5.95 6.79h2a1 1 0 0 0 0-2z"/><path d="m23 11.24a6.16 6.16 0 0 0 -6.24-5.24h-1.51c-.81 0-1.25.45-1.25 1a1 1 0 0 0 1 1h1.79a4.13 4.13 0 0 1 4.21 3.63 4 4 0 0 1 -4 4.37h-2a1 1 0 0 0 0 2h2a6 6 0 0 0 6-6.76z"/></g></svg>
+							<a href="https://lusithanddevices.com/" target="_blank">https://lusithanddevices.com/</a>
+						</p>
+					</div>
+
+					<div class="website threecol">
+						<div id="your-site-here">
+							<p><a href="/contact">Your Site Here</a></p>
+						</div>
+						<p>
+							Like what you see here and think we could make for a successful team?
+						</p>
+						<p class="website-url"><a href="/contact">Let's Talk!</a></p>
+					</div>
+
+
 				</div>
 
-
 				<br/>
 				<br/>
 
-				<p>Take a look at <a href="/cv">my CV</a> to have a better sense of the technologies used.</p>
+				<p>Take a look at <a href="https://fredrocha.net/wp-content/uploads/2022/09/Fred-Rocha-Web-Engineer-with-Eye-for-Design-September-2022.pdf">my CV</a> to have a better sense of the technologies used.</p>
 
 
 		</div> <?php // end #inner-content ?>
