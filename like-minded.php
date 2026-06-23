6c39b9427c9ccfa47f64578298768a621c4a524c
john_fisherman <john.fisherman@gmail.com>
2026-06-23 17:42:45 +0100

    Add friends to like-minded.

--- like-minded.php
+++ like-minded.php
@@ -18,12 +18,19 @@ Template Name: Like-minded
 
 				<p class="like-minded-intro">The personal websites I list here will amuse you and teach you stuff, expand your mind and leave you scratching your head. Visit them, sign-up to their (RSS) feed, keep coming back.</p>
 				
-				<p>The rationale for linking to other sites in 2023 is <a href="https://fredrocha.net/2023/04/21/build-your-own-website-and-they-will-not-come/">expanded on this post</a>. Go ahead and do some linking of your own, why won't ya?</p>
+				<p>The rationale for linking to other sites in 2023 is <a href="https://fredrocha.net/2023/04/21/build-your-own-website-and-they-will-not-come/">expanded on this post</a>. Go ahead and do clicking and some linking of your own, why won't ya?</p>
 				
-				<h2>Close friends</h2>
+				<h2>Friends IRL</h2>
 				
 				<ul>
-				
+					<li>
+						<a href="https://sergiosantos.info/" rel="friend" target="_blank">Sérgio Santos</a> - Builder with a focus on Android, running <a href="https://bloco.io" target="_blank">Bloco Studio</a> with Claudia Acabado. <a href="https://sergiosantos.info/index.xml" target="_blank">RSS feed</a>. <a href="https://mastodon.social/@sdsantos" target="_blank">Mastodon</a>.
+					</li>
+
+					<li>
+						<a href="https://catarinasobral.com" rel="friend" target="_blank">Catarina Sobral</a> - Visual artist from Coimbra to the world. Please check her work out, and specifically on female artists that shaped the portuguese creative landscape.
+					</li>
+
 					<li>
 						<a href="https://jamez.it" rel="friend" target="_blank">Giacomo Miceli aka jamez</a> - an artist displaying some of his wonderfully thoughtful work. You might know him from his <a href="https://infiniteconversation.com/" target="_blank">infinite conversation</a> between Herzog and Žižek. <a href="https://www.jamez.it/blog/feed/" target="_blank">RSS feed</a>.
 					</li>
@@ -34,9 +41,22 @@ Template Name: Like-minded
 					<li>
 						<a href="https://www.arianekensa.com/" rel="friend" target="_blank">Ariane Kensa</a> - she makes outstanding urban art and sleek design.
 					</li>
-					
 				</ul>
 				
+				<h2 id="webizens-sites">Webizens <a href="#webizens-sites">#</a></h2>
+				
+				<ul>
+					<li>
+						<a href="https://desmondrivet.com/" target="_blank">Desmond Rivet</a> - Displaying his delicious recipes and sharp photo albums. Check his work out! <a href="https://desmondrivet.com/recipes/feed.xml" target="_blank">Food RSS feed</a>.
+					</li>
+					<li>
+						<a href="https://oldrup.dk/" target="_blank">Bjarne Oldrup</a> - fellow WordPress builder, Bjarne has a strong focus on environmental responsibility and website performance. An extra reason to read all the posts! <a href="https://oldrup.dk/en/feed/" target="_blank">RSS feed</a>.
+					</li>
+					<li>
+						<a href="https://abalburdia.pt" target="_blank">João Azevedo</a> - [PT] Freestyle thoughts and ideas from an old school webizen, now back to expand his craft. <a href="https://abalburdia.pt/feed" target="_blank">RSS feed</a>.
+					</li>
+				</ul>
+
 				<h2 id="btconf-sites">Beyond Tellerrand Folks <a href="#btconf-sites">#</a></h2>
 				
 				<p>
@@ -68,20 +88,6 @@ Template Name: Like-minded
 					<li>
 						<a href="https://wentsch.me" target="_blank">Daniel Wentsch</a> - A designer & developer keeping a journal of what he finds while exploring the world. <a href="https://wentsch.me/rss/" target="_blank">RSS feed</a>.
 					</li>
-					
-				</ul>
-				
-				<h2 id="webizens-sites">Webizens <a href="#webizens-sites">#</a></h2>
-				
-				
-				<ul>
-					<li>
-						<a href="https://desmondrivet.com/" target="_blank">Desmond Rivet</a> - Displaying his delicious recipes and sharp photo albums. Check his work out! <a href="https://desmondrivet.com/recipes/feed.xml" target="_blank">Food RSS feed</a>.
-					</li>
-					<li>
-						<a href="https://oldrup.dk/" target="_blank">Bjarne Oldrup</a> - fellow WordPress builder, Bjarne has a strong focus on environmental responsibility and website performance. An extra reason to read all the posts! <a href="https://oldrup.dk/en/feed/" target="_blank">RSS feed</a>.
-					</li>
-					
 				</ul>
 			
 				<p>OK, now <a href="https://localghost.dev/blog/building-a-website-like-it-s-1999-in-2022" target="_blank">go build your own</a>. And let me know if you get stuck.</p>
@@ -92,8 +98,6 @@ Template Name: Like-minded
 				<br/>
 
 			</div><!-- #main -->
-
-
 
 			<br/>
 			<br/>
